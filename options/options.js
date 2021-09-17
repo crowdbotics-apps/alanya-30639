fac9c951ccb4fdd5393d95c9b18b0774539e11c2
Crowdbotics <team@crowdbotics.com>
2021-09-17 15:35:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "alanya_30639Identifier",
+  url: "https://alanya_30639Identifier.botics.co",
+  api: "https://alanya_30639Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
